4cde48d3e472cdaa969855c63b34db2485eda3b9
janarthanan <janarthanan0607@gmail.com>
2024-12-10 11:26:59 +0530

    initial commit

---
+++ src/main/java/com/example/schoolmanagement/util/AuthenticationService.java
@@ -0,0 +1,16 @@
+package com.example.schoolmanagement.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContextUtil {
+    public String getCurrentUser() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            return authentication.getName();
+        }
+        return Constants.NOT_FOUND;
+    }
+}
